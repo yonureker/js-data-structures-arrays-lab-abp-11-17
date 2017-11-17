594cb9041f8e6561b5b2982f8e2f52e7ea031d44
Onur Eker <yonureker@gmail.com>
2017-11-17 17:26:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function prependDriver(name){
 }
 
 function removeLastDriver(){
-  return newDrivers = drivers.splice(drivers.lenght - 1,1)
+  return newDrivers = drivers.slice(drivers.length - 1)
 }
